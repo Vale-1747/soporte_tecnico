82bd60f57322bf0ab8ac54a55b62c175b700d960
santiagorub <santiago.opazo@uap.edu.ar>
2026-06-16 00:50:06 -0300

    se inicio con la gestion de tecnicos: Tecnico.cs, nTecnico.cs y frmTecnico.cs

--- obj/Debug/net10.0-windows/soporte_tecnico.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/soporte_tecnico.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("soporte_tecnico")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89c59770a3b34e939c84b8efda170cae7f5bb3bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("soporte_tecnico")]
 [assembly: System.Reflection.AssemblyTitleAttribute("soporte_tecnico")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
